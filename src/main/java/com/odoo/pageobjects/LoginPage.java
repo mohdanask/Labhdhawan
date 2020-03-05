5f9f208e8910d0ce67c7b14aeab89dcc3053773e
Digi27 <diganta.sdp@gmail.com>
2020-03-05 13:40:19 +0530

    intentional error resolve

--- src/main/java/com/odoo/pageobjects/LoginPage.java
+++ src/main/java/com/odoo/pageobjects/LoginPage.java
@@ -5,7 +5,5 @@ public class LoginPage
 {
 	public String unTxtBx="//input[@id='login']";
 	public String pwdTxtBx="//input[@id='password']";
-	public String loginBtn="//button[text()='Log']";
-	
-	// in
+	public String loginBtn="//button[text()='Log in']";
 }
